8af9edac2e972fd6dad5fc3faa9874030802d006
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-22 01:02:47 -0500

    feat(root): Nuevas rutas
    Se configuraron las rutas para citas y medicos

--- src/routers/appointment.route.js
+++ src/routers/appointment.route.js
@@ -2,11 +2,26 @@ import { Router } from "express";
 import appointments from "../storage/appointment.js";
 import limit from "../limits/setting.limits.js"
 import { validateToken } from "../middleware/validateTokens/jwtVerify.js"
-import {getAppointments, getAppointmentsForUserID} from "../controllers/consults/appointment.js";
+// controllers
+import {getAppointments, 
+        getAppointmentsForUserID, 
+        getAppointmentsForDoctor,
+        getAppointmentForconsultingRoom,
+        getAppointmentForDate,
+        getAppointmentsForDateAndDoctor,
+        getConsultingRooms,
+        getAppointmentsForGenderAndStatus
+} from "../controllers/consults/appointment.js";
 
 const appointment = Router(); 
 
 appointment.get("/citas", limit, validateToken(appointments), getAppointments)
 appointment.get("/citas/:paciente", limit, validateToken(appointments), getAppointmentsForUserID)
+appointment.get("/citas/medico/:medico", limit, validateToken(appointments), getAppointmentsForDoctor)
+appointment.get("/citas/consultorio/:paciente", limit, validateToken(appointments), getAppointmentForconsultingRoom)
+appointment.get("/fecha/cita", limit, validateToken(appointments), getAppointmentForDate)
+appointment.get("/cantidad/citas", limit, validateToken(appointments), getAppointmentsForDateAndDoctor)
+appointment.get("/consultorios/citas", limit, validateToken(appointments), getConsultingRooms)
+appointment.get("/genero/citas/:genero", limit, validateToken(appointments), getAppointmentsForGenderAndStatus)
 
 export default appointment; 
--- src/routers/doctor.router.js
+++ src/routers/doctor.router.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import doctors from "../storage/doctors.js";
 import limit from "../limits/setting.limits.js"; 
-import getDoctorForSpecialization from "../controllers/consults/doctors.js";
 import { validateToken } from "../middleware/validateTokens/jwtVerify.js";
+// controllers
+import {getDoctorForSpecialization,
+       getDoctorsAndSpecializations} 
+from "../controllers/consults/doctors.js";
 
 const doctor = Router(); 
 
 doctor.get("/doctores/:especialidad", limit, validateToken(doctors), getDoctorForSpecialization)
+doctor.get("/doctores/obtener/especializacion", limit, validateToken(doctors), getDoctorsAndSpecializations)
 
 export default doctor; 
