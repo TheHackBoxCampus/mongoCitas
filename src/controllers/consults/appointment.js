c218de84915faec2aaa5428f766740f879717a85
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-22 01:01:16 -0500

    feat(root): Consultas de citas
    se realizaron las consultas pertinentes para las citas

--- src/controllers/consults/appointment.js
+++ src/controllers/consults/appointment.js
@@ -1,69 +1,333 @@
 import conx from "../../config/db.js";
 
-const getAppointments = async(req, res) => {
-    try {
-        let db = await conx(); 
-        let collection = await db.collection("cita"); 
-        let consult = await collection.find().sort({fecha: 1}).toArray(); 
-        return res.status(200).send(consult); 
-    }catch (err) {
-        res.sendStatus(500)
-        console.log(err)
-    }
-}
+const getAppointments = async (req, res) => {
+  try {
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection.find().sort({ fecha: 1 }).toArray();
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
 
-const getAppointmentsForUserID = async(req, res) => {
-    try {
-        let patientId = parseInt(req.params.paciente); 
-        let db = await conx(); 
-        let collection = await db.collection("cita"); 
-        let consult = await collection.aggregate([
-            {
-                $match: {
-                    usuario: patientId
-                }
-            },
-            {
-                $lookup: {
-                    from: "usuario",
-                    localField: "usuario",
-                    foreignField: "_id",
-                    as: "datos_usuario"
-                }
-            },
-            {
-                $unwind: "$datos_usuario"
-            },
-            {
-                $lookup: {
-                    from: "medico",
-                    localField: "medico",
-                    foreignField: "_id",
-                    as: "datos_medico"
-                }
-            },
-            {
-                $unwind: "$datos_medico"
-            },
-            {
-                $project: {
-                    _id: 0,
-                    datos_usuario: {nombres:1, apellidos: 1},
-                    datos_medico: {nombreCompleto: 1},
-                    fecha: 1
-                }
-            }
-        ]).toArray(); 
-        return res.status(200).send(consult); 
-    }catch (err) {
-        res.sendStatus(500)
-        console.log(err)
-    }
-}
+const getAppointmentsForUserID = async (req, res) => {
+  try {
+    let patientId = parseInt(req.params.paciente);
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $match: {
+            usuario: patientId,
+          },
+        },
+        {
+          $lookup: {
+            from: "usuario",
+            localField: "usuario",
+            foreignField: "_id",
+            as: "datos_usuario",
+          },
+        },
+        {
+          $unwind: "$datos_usuario",
+        },
+        {
+          $lookup: {
+            from: "medico",
+            localField: "medico",
+            foreignField: "_id",
+            as: "datos_medico",
+          },
+        },
+        {
+          $unwind: "$datos_medico",
+        },
+        {
+          $project: {
+            _id: 0,
+            datos_usuario: { nombres: 1, apellidos: 1 },
+            datos_medico: { nombreCompleto: 1 },
+            fecha: 1,
+          },
+        },
+      ])
+      .toArray();
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
 
+const getAppointmentsForDoctor = async (req, res) => {
+  try {
+    let doctorId = parseInt(req.params.medico);
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $match: {
+            medico: doctorId,
+          },
+        },
+        {
+          $lookup: {
+            from: "medico",
+            localField: "medico",
+            foreignField: "_id",
+            as: "medico",
+          },
+        },
+        {
+          $unwind: "$medico",
+        },
+        {
+          $project: {
+            _id: 0,
+            medico: { nombreCompleto: 1 },
+            fecha: 1,
+          },
+        },
+      ])
+      .toArray();
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
 
+const getAppointmentForconsultingRoom = async (req, res) => {
+  try {
+    let patientId = parseInt(req.params.paciente);
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $match: {
+            usuario: patientId,
+          },
+        },
+        {
+          $lookup: {
+            from: "usuario",
+            localField: "usuario",
+            foreignField: "_id",
+            as: "datos_usuario",
+          },
+        },
+        {
+          $unwind: "$datos_usuario",
+        },
+        {
+          $lookup: {
+            from: "medico",
+            localField: "medico",
+            foreignField: "_id",
+            as: "datos_medico",
+          },
+        },
+        {
+          $unwind: "$datos_medico",
+        },
+        {
+          $lookup: {
+            from: "consultorio",
+            localField: "datos_medico.consultorio",
+            foreignField: "_id",
+            as: "consultorio",
+          },
+        },
+        {
+          $unwind: "$consultorio",
+        },
+        {
+          $project: {
+            _id: 0,
+            datos_usuario: { nombres: 1, apellidos: 1 },
+            consultorio: { codigo: 1 },
+            fecha: 1,
+          },
+        },
+      ])
+      .toArray();
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
+
+const getAppointmentForDate = async (req, res) => {
+  try {
+    let { fecha } = req.body;
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $match: {
+            fecha: fecha,
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            fecha: 1,
+            medico: 1,
+            usuario: 1,
+          },
+        },
+      ])
+      .toArray();
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
+
+const getAppointmentsForDateAndDoctor = async (req, res) => {
+  try {
+    let { fecha, medico } = req.body;
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $match: {
+            medico,
+            fecha,
+          },
+        },
+        {
+          $count: "totalCitas",
+        },
+      ])
+      .toArray();
+    return res
+      .status(200)
+      .send({ id_medico: medico, cantidad: consult[0].totalCitas });
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
+
+const getConsultingRooms = async (req, res) => {
+  try {
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $lookup: {
+            from: "consultorio",
+            localField: "medico",
+            foreignField: "_id",
+            as: "consultorios",
+          },
+        },
+        {
+          $unwind: "$consultorios",
+        },
+        {
+          $group: {
+            _id: "$usuario",
+            consultorios: {
+              $addToSet: "$consultorios.codigo",
+            },
+          },
+        },
+      ])
+      .toArray();
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
+
+const getAppointmentsForGenderAndStatus = async (req, res) => {
+  try {
+    let genero = req.params.genero;
+    let db = await conx();
+    let collection = await db.collection("cita");
+    let consult = await collection
+      .aggregate([
+        {
+          $lookup: {
+            from: "usuario",
+            localField: "usuario",
+            foreignField: "_id",
+            as: "paciente",
+          },
+        },
+        {
+          $unwind: "$paciente",
+        },
+        {
+          $lookup: {
+            from: "genero", 
+            localField: "paciente.genero", 
+            foreignField: "_id",
+            as: "genero",
+          },
+        },
+        {
+          $unwind: "$genero",
+        },
+        {
+          $lookup: {
+            from: "estado_cita",
+            localField: "estado",
+            foreignField: "_id",
+            as: "estado"
+          }
+        },
+        {
+          $unwind: "$estado",
+        },
+        {
+          $match: {
+            "genero.nombre": genero, 
+            "estado.nombre": "Atendida"
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            fecha: 1,
+            paciente: {
+              _id: "$paciente._id",
+              nombre: "$paciente.nombre",
+              estado: "$estado.nombre"
+            },
+          },
+        },
+      ])
+      .toArray();
+
+    return res.status(200).send(consult);
+  } catch (err) {
+    res.sendStatus(500);
+    console.log(err);
+  }
+};
 
 export {
-    getAppointments,
-    getAppointmentsForUserID
-}; 
+  getAppointments,
+  getAppointmentsForUserID,
+  getAppointmentsForDoctor,
+  getAppointmentForconsultingRoom,
+  getAppointmentForDate,
+  getAppointmentsForDateAndDoctor,
+  getConsultingRooms,
+  getAppointmentsForGenderAndStatus,
+};
