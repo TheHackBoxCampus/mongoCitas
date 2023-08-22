f13ed64498c48c58c40f38015254ead50800fff9
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-22 01:01:43 -0500

    feat(root): Consultas para los medicos
    se realizaron las consultas pertinentes para los medicos

--- src/controllers/consults/doctors.js
+++ src/controllers/consults/doctors.js
@@ -39,4 +39,39 @@ const getDoctorForSpecialization = async(req, res) => {
     }
 }
 
-export default getDoctorForSpecialization; 
+const getDoctorsAndSpecializations = async (req, res) => {
+    try {
+        let db = await conx(); 
+        let collection = await db.collection("medico");
+        console.log("re")
+        let consult = await collection.aggregate([
+            {
+                $lookup: {
+                    from: "consultorio",
+                    localField: "consultorio",
+                    foreignField: "_id",
+                    as: "consultorio"
+                }
+            },
+            {
+                $unwind: "$consultorio"
+            },
+            {
+                $project: {
+                    _id: 0,
+                    nombreCompleto: 1,
+                    consultorio: {codigo:1}
+                }
+            }
+        ]).toArray(); 
+        return res.status(200).send(consult); 
+    }catch (err){
+        res.sendStatus(500);
+        console.log(err)
+    }
+}
+
+export {
+    getDoctorForSpecialization,
+    getDoctorsAndSpecializations
+}
