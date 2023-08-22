d0465ecb70c905de73233fd466cbdaf704f837ab
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-21 23:08:28 -0500

    feat(root): Validacion de tokens
    Se realizo la respectiva funcion para la validacion de los tokens con el passport autheticate y el jsonwebtoken, con el registro en el payload se evalua la sentencia con una referencia creada y certifica, en caso de que sea correcto retorna true, en caso contrario false.
    
    Para el acceso de los recursos de los endpoints es necesario el true

---
+++ src/middleware/validateTokens/jwtVerify.js
@@ -0,0 +1,26 @@
+import { classToPlain, plainToClass } from "class-transformer";
+import passport from "../../passport/settings.passport.js";
+
+const isEqual = (cls, decoded) => {
+    let ref = JSON.stringify({...decoded})
+    let copy = JSON.stringify(classToPlain(plainToClass(cls, {}, {ignoreDecorators: true})));
+    let verify = copy === ref; 
+    return verify
+}
+
+const validateToken = (cls) => (req, res, next) => {
+    passport.authenticate("bearer", {session: false}, (err, decoded) => {
+        if(err) res.status(401).send({status: 401, message: "Erro en la autenticacion!"})
+        else {
+            let filter = Object.entries(decoded).map(p => p[0] != "iat" && p[0] != "exp" ? p : false).filter(Boolean); 
+            let reformate = Object.fromEntries(filter); 
+            let response = isEqual(cls, reformate);
+            // responses
+            return response ? next() : res.status(403).send({status: 403, message: "Token invalido!"})
+        }
+    })(req, res, next)
+}
+
+export {
+    validateToken
+}
