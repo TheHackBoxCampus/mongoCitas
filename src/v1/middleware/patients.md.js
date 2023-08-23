81b50176bec2cc4fdaed666c5c644a99fa0ebf7b
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-23 09:14:11 -0500

    !feat(root): Middleware para la ruta de pacientes
    TIene errores de logica y estan a la disposicion de cambios

---
+++ src/v1/middleware/patients.md.js
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import patients from "../storage/users.js";
+import {validate} from "class-validator"
+
+const patients_md = async (req, res, next) => {
+    try {
+        if(Object.entries(req.body) < 1) throw "Parametros necesarios"
+        else {
+            const instance = plainToClass(patients, req.body);
+            await validate(instance);
+      
+          
+            return next();
+        }
+    }catch(err) {
+        return res.status(500).send({status: 500, message: err})
+    }
+}
+
+export {
+    patients_md,
+}
